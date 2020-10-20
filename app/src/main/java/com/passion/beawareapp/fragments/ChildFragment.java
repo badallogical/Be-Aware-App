00f425273e2020dd3235804978bcbad3b6a1929d
narayan <badallearn@gmail.com>
2020-10-20 11:43:38 +0530

    added image loading with glide

--- app/src/main/java/com/passion/beawareapp/fragments/ChildFragment.java
+++ app/src/main/java/com/passion/beawareapp/fragments/ChildFragment.java
@@ -15,10 +15,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.bumptech.glide.Glide;
 import com.passion.beawareapp.R;
 import com.passion.beawareapp.models.News;
 import com.passion.beawareapp.utils.DownloadImageTask;
-
 import org.w3c.dom.Text;
 
 public class ChildFragment extends Fragment {
@@ -27,13 +27,11 @@ public class ChildFragment extends Fragment {
     ImageView image;
     TextView desc;
     TextView read_more;
-
     News news;
 
     public ChildFragment( News news ){
         this.news = news;
     }
-
 
     @Override
     public View onCreateView( LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
@@ -44,13 +42,12 @@ public class ChildFragment extends Fragment {
         title.setText( news.getTitle() );
 
         image = (ImageView) v.findViewById(R.id.image_id);
-        new DownloadImageTask(image).execute(news.getUtl_to_img());
+        Glide.with(this).load(news.getUtl_to_img()).placeholder(R.drawable.be_aware).into(image);
 
         desc = (TextView) v.findViewById(R.id.news_desc);
         desc.setText( news.getDesc() + news.getContent());
 
         read_more = (TextView) v.findViewById(R.id.read_more);
-
         final News tnews = news;
         read_more.setOnClickListener(new View.OnClickListener() {
             @Override
